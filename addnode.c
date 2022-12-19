a814ffee5a38c987d598d10c59e53d1dc0d799ca
Fikayomi31 <fikayoogundijo@gmail.com>
2022-12-18 23:46:55 -0800

    addnotefile

--- addnode.c
+++ addnode.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
  * addnode - add node to the head stack
  * @head: head of the stack
  * @n: new_value
  * Return: no return
 */
+
 void addnode(stack_t **head, int n)
 {
 
